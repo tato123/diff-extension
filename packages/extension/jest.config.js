bd19e1e15ca68b3824a2f4332cb9070e42b13b57
Jonathan Fontanez <fontanezj1@gmail.com>
2018-09-08 12:50:02 -0400

    moving config to fix testing on ci/cd

--- packages/extension/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  verbose: true,
-  transform: {
-    "^.+\\.jsx?$": "babel-jest",
-    "^.+\\.tsx?$": "ts-jest"
-  },
-  moduleDirectories: ["node_modules", "<rootDir>/node_modules"],
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"]
-};
