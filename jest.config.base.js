a3b73be58e28213fc43829b771281dcdc20c1512
Jonathan Fontanez <fontanezj1@gmail.com>
2018-09-08 14:02:02 -0400

    updating the location of ts-jest

--- jest.config.base.js
+++ jest.config.base.js
@@ -2,7 +2,7 @@ module.exports = {
   verbose: true,
   transform: {
     "^.+\\.jsx?$": "babel-jest",
-    "^.+\\.tsx?$": "ts-jest"
+    "^.+\\.tsx?$": "<rootDir>/../../node_modules/ts-jest/preprocessor.js"
   },
   moduleDirectories: ["node_modules", "<rootDir>/node_modules"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"]
