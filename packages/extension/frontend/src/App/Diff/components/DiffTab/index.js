71ae654d95b42dc39edd3fe164e7d0b466baa74c
Jonathan Fontanez <fontanezj1@gmail.com>
2018-09-13 15:21:09 -0400

    updates to the public site

--- packages/extension/frontend/src/App/Diff/components/DiffTab/index.js
+++ packages/extension/frontend/src/App/Diff/components/DiffTab/index.js
@@ -1,7 +1,47 @@
 import React from "react";
+import { Header, Button } from "@diff/shared-components";
+import styled from "styled-components";
 
-export default class Assets extends React.PureComponent {
+import diff from "./diff.png";
+
+const Outlined = styled.div`
+  border: 2px dashed rgba(255, 255, 255, 0.5);
+  border-radius: 10px;
+  margin-bottom: 16px;
+`;
+
+const CustomHeader = styled(Header)`
+  color: rgb(67, 202, 217);
+  font-weight: 500 !important;
+  text-align: center;
+`;
+
+const Container = styled.div`
+  text-align: center;
+  margin-bottom: 16px;
+`;
+
+export default class Diffs extends React.PureComponent {
   render() {
-    return <div>Nothing to Display</div>;
+    return (
+      <div>
+        <CustomHeader as="h2" uppercase>
+          Comming soon
+        </CustomHeader>
+        <Outlined>
+          <img src={diff} />
+        </Outlined>
+
+        <Container>
+          <small>
+            Tell us what you think about this feature and how you might improve
+            it?
+          </small>
+        </Container>
+        <Container>
+          <Button>GIVE US FEEDBACK</Button>
+        </Container>
+      </div>
+    );
   }
 }
