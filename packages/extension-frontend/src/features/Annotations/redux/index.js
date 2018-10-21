3b4c4196dca70751281e9aaec05894de8e57acb5
Jonathan Fontanez <fontanezj1@gmail.com>
2018-10-21 16:47:42 -0400

    updating paths

--- packages/extension-frontend/src/features/Annotations/redux/index.js
+++
@@ -1,4 +0,0 @@
-export { default as epics } from './epics';
-export { default as actions } from './actions';
-export { default as selectors } from './selectors';
-export { default as types } from './types';
