8305d060af33a2e53594bb98155f0676326abd64
Jonathan Fontanez <fontanezj1@gmail.com>
2018-12-03 18:59:30 -0500

    updating data

--- packages/graphql-firestore/utils/collections.js
+++
@@ -1,8 +0,0 @@
-module.exports.allCollection = (db, collectionName) => async () => {
-    const events = [];
-    const querySnapshot = await db.collection(collectionName).get();
-    querySnapshot.forEach(doc => {
-        events.push(doc.data())
-    })
-    return events;
-}
